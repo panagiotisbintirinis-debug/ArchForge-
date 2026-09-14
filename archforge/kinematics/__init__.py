7b8ea61965829626eeb8e4fe9e1712a23f1a426d
panagiotisbintirinis-debug <panagiotisbintirinis@gmail.com>
2026-09-14 08:25:58 +0300

    Expose embedded mechanism cavity integration API

--- archforge/kinematics/__init__.py
+++ archforge/kinematics/__init__.py
@@ -1,5 +1,9 @@
 """Kinematic design foundations for movable architectural assemblies."""
 
 from .model import JOINT_TYPES, assembly_parts, clamp_joint_value, joint_state, mechanism_envelope
+from .integration import cavity_modifier_for_mount, ensure_mount_cavity
 
-__all__ = ['JOINT_TYPES','assembly_parts','clamp_joint_value','joint_state','mechanism_envelope']
+__all__ = [
+    'JOINT_TYPES','assembly_parts','clamp_joint_value','joint_state','mechanism_envelope',
+    'cavity_modifier_for_mount','ensure_mount_cavity',
+]
