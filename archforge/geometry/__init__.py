9ff9da5620c303827a9841004d389f244b519ca5
panagiotisbintirinis-debug <panagiotisbintirinis@gmail.com>
2026-09-14 09:43:31 +0300

    Expose tessellation picking and sculpt preview geometry APIs

--- archforge/geometry/__init__.py
+++ archforge/geometry/__init__.py
@@ -3,6 +3,9 @@ from .plan import EvaluationNode, EvaluationPlan, build_evaluation_plan, fabrica
 from .surfaces import SurfaceDescriptor, surface_catalog, surface_roles, resolve_surface
 from .backend import GeometryBackend, GeometryBody, GeometryEvaluation, GeometryIssue, ContractBackend
 from .preview import PreviewBackend, PreviewPayload
+from .mesh import MeshPayload, TessellatedPreviewBackend
+from .sculpt import SculptedPreviewBackend, apply_brush_modifier
+from .picking import MeshRayHit, raycast_mesh, raycast_evaluation, surface_hit_from_raycast
 from .fabrication import FabricationFinding, FabricationReport, assess_fabrication, require_fabrication_ready
 from .selection import SurfaceHit, BrushSpec, sculpt_modifier_from_hit
 
@@ -10,6 +13,8 @@ __all__=[
  'EvaluationNode','EvaluationPlan','build_evaluation_plan','fabrication_candidates',
  'SurfaceDescriptor','surface_catalog','surface_roles','resolve_surface',
  'GeometryBackend','GeometryBody','GeometryEvaluation','GeometryIssue','ContractBackend',
- 'PreviewBackend','PreviewPayload','FabricationFinding','FabricationReport',
- 'assess_fabrication','require_fabrication_ready','SurfaceHit','BrushSpec','sculpt_modifier_from_hit',
+ 'PreviewBackend','PreviewPayload','MeshPayload','TessellatedPreviewBackend',
+ 'SculptedPreviewBackend','apply_brush_modifier','MeshRayHit','raycast_mesh','raycast_evaluation',
+ 'surface_hit_from_raycast','FabricationFinding','FabricationReport','assess_fabrication',
+ 'require_fabrication_ready','SurfaceHit','BrushSpec','sculpt_modifier_from_hit',
 ]
