3992b486e88987c3abbc07ae0ca957e6f430d5ea
panagiotisbintirinis-debug <panagiotisbintirinis@gmail.com>
2026-09-14 09:06:52 +0300

    Expose semantic surface picking and brush intent

--- archforge/geometry/__init__.py
+++ archforge/geometry/__init__.py
@@ -4,11 +4,12 @@ from .surfaces import SurfaceDescriptor, surface_catalog, surface_roles, resolve
 from .backend import GeometryBackend, GeometryBody, GeometryEvaluation, GeometryIssue, ContractBackend
 from .preview import PreviewBackend, PreviewPayload
 from .fabrication import FabricationFinding, FabricationReport, assess_fabrication, require_fabrication_ready
+from .selection import SurfaceHit, BrushSpec, sculpt_modifier_from_hit
 
 __all__=[
  'EvaluationNode','EvaluationPlan','build_evaluation_plan','fabrication_candidates',
  'SurfaceDescriptor','surface_catalog','surface_roles','resolve_surface',
  'GeometryBackend','GeometryBody','GeometryEvaluation','GeometryIssue','ContractBackend',
  'PreviewBackend','PreviewPayload','FabricationFinding','FabricationReport',
- 'assess_fabrication','require_fabrication_ready',
+ 'assess_fabrication','require_fabrication_ready','SurfaceHit','BrushSpec','sculpt_modifier_from_hit',
 ]
