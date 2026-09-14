0ac3cb871b4d0cf59f8d54727c803b0a02dbb348
panagiotisbintirinis-debug <panagiotisbintirinis@gmail.com>
2026-09-15 00:33:08 +0300

    Export semantic organic junction APIs

--- archforge/organic/__init__.py
+++ archforge/organic/__init__.py
@@ -5,17 +5,22 @@ from .biospectre import (
     profile_radius,
     overlap,
     junction_plane,
+    junction_section_polygon,
     junction_key_for_pods,
     find_pod_junctions,
     all_pod_junctions,
 )
+from .junctions import JunctionIntentResult, infer_organic_junctions
 
 __all__ = [
     'shell_top',
     'profile_radius',
     'overlap',
     'junction_plane',
+    'junction_section_polygon',
     'junction_key_for_pods',
     'find_pod_junctions',
     'all_pod_junctions',
+    'JunctionIntentResult',
+    'infer_organic_junctions',
 ]
