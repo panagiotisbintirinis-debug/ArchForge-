2d73fc826570adac6d5f760dd59164f3169f1ed2
panagiotisbintirinis-debug <panagiotisbintirinis@gmail.com>
2026-09-14 08:18:22 +0300

    Add kinematic subsystem package

---
+++ archforge/kinematics/__init__.py
@@ -0,0 +1,5 @@
+"""Kinematic design foundations for movable architectural assemblies."""
+
+from .model import JOINT_TYPES, assembly_parts, clamp_joint_value, joint_state, mechanism_envelope
+
+__all__ = ['JOINT_TYPES','assembly_parts','clamp_joint_value','joint_state','mechanism_envelope']
